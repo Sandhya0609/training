149fa954855c8922bb611bb941ab225060853dae
Sandhya0609 <sandhya0609a@gmail.com>
2023-05-26 19:23:28 -0400

    Adding addallvalues changes

--- src/main/java/basics/core/AddAllValues.java
+++ src/main/java/basics/core/AddAllValues.java
@@ -1,18 +1,21 @@
 package basics.core;
+
+import java.util.Scanner;
+
 //Write a program to add all the values in a given number and print.
 // Ex: 1234->10 For Loop
 public class AddAllValues {
-    /*public static void main(String arg[])
+    public static void main(String arg[])
     {
-        long n,sum;
+
         Scanner sc=new Scanner(System.in);
         System.out.println("Enter a number ");
-        n=sc.nextLong();
-        for(sum=0 ;n!=0 ;n/=10)
+        long num,sum;
+        num=sc.nextLong();
+        for(sum=0 ;num!=0 ;num/=10)
         {
-            sum+=n%10;
+            sum+=num%10;
         }
-        System.out.println("Sum of digits of a number is "+sum);
+        System.out.println("Sum of digits of a number is: "+sum);
     }
-}*/
 }
